27be82a0d5f8659dbae0caedcc87f95ad9defa84
Lolaev Sergey <lolaevgasu@gmail.com>
2022-07-16 13:28:22 +0300

    initial commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by admin on 15.07.2022.
+//  Copyright © 2022 Sergey Lolaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
